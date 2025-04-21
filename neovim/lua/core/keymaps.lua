147145e5c253007bfbede59d1c3a817855febb00
lanzmoncis <lanzmoncis@gmail.com>
2025-04-21 04:46:56 +0100

    update nvim and tmux conf

---
+++ neovim/lua/core/keymaps.lua
@@ -0,0 +1,18 @@
+-- Set leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Disable the spacebar key's default behavior in Normal and Visual modes
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- For conciseness
+local opts = { noremap = true, silent = true }
+
+-- delete single character without copying into register
+vim.keymap.set("n", "x", '"_x', opts)
+
+-- Toggle line wrapping
+vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts)
+
+-- Open diagnostic float window
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
