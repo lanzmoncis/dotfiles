9d5eaa9ca1584edb1cc1586db417230e8a4974a3
lanzmoncis <lanzmoncis@gmail.com>
2025-09-13 19:42:51 +0100

    Add tiny-inline diagnostics

--- neovim/lua/plugins/misc.lua
+++ neovim/lua/plugins/misc.lua
@@ -113,4 +113,13 @@ return {
 		opts = {},
 		lazy = true,
 	},
+	{
+		"rachartier/tiny-inline-diagnostic.nvim",
+		event = "VeryLazy",
+		priority = 1000,
+		config = function()
+			require("tiny-inline-diagnostic").setup()
+			vim.diagnostic.config({ virtual_text = false }) -- Disable default virtual text
+		end,
+	},
 }
